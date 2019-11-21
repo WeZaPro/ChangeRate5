9f330cb161636666728a8407c2993b87ac1bf8a8
taweesak <taweesak9359@gmail.com>
2019-11-21 14:12:38 +0700

    update from office1

--- app/src/main/java/taweesak/com/changerate5/FirstFragment.java
+++ app/src/main/java/taweesak/com/changerate5/FirstFragment.java
@@ -25,13 +25,13 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
 
     Button btn;
     EditText editText;
-    TextView txtView,tvChoice;
+    TextView txtView, tvChoice;
     ImageView mvChoice;
 
     RadioGroup radioGroup;
     RadioButton radioButton;
 
-    int selectId,choice;
+    int selectId, choice;
 
     @Override
     public void onAttach(Context context) {
@@ -67,9 +67,9 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
     }
 
     private void convertValue() {
-        selectId =radioGroup.getCheckedRadioButtonId();
+        selectId = radioGroup.getCheckedRadioButtonId();
         radioButton = getActivity().findViewById(selectId);
-        switch (radioButton.getId()){
+        switch (radioButton.getId()) {
             case R.id.zeroText:
                 choice = 0;
                 break;
@@ -81,11 +81,41 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
                 break;
         }
 
-        double valueRate = Double.parseDouble(editText.getText().toString());
-        Model model = new Model(choice,valueRate);
+        try {
+            double valueRate = Double.parseDouble(editText.getText().toString());
+            if (valueRate > 0) {
+                Model model = new Model(choice, valueRate);
+                txtView.setText("rate = " + model.getRates());
+                tvChoice.setText(model.getRateText());
+                Toast.makeText(getActivity(), "Message " + choice, Toast.LENGTH_LONG).show();
 
-        txtView.setText("rate = "+model.getRates());
-        Toast.makeText(getActivity(),"Message "+choice ,Toast.LENGTH_LONG).show();
+                changeFlag(model);
+
+            } else {
+                txtView.setText("ค่าที่ใส่ต้องมากกว่า 0 เท่านั้น");
+                tvChoice.setText("");
+                mvChoice.setImageResource(R.drawable.my_image2);
+            }
+        } catch (NumberFormatException e) {
+            txtView.setText("ค่าที่ใส่ต้องเป็นตัวเลข เท่านั้น");
+            tvChoice.setText("");
+            mvChoice.setImageResource(R.drawable.my_image2);
+        }
+
+    }
+
+    private void changeFlag(Model model) {
+        switch (model.getChoice()) {
+            case 0:
+                mvChoice.setImageResource(R.drawable.usa);
+                break;
+            case 1:
+                mvChoice.setImageResource(R.drawable.japan);
+                break;
+            case 2:
+                mvChoice.setImageResource(R.drawable.europe);
+                break;
+        }
     }
 
     private void findView(View view) {
@@ -94,7 +124,7 @@ public class FirstFragment extends Fragment implements View.OnClickListener {
         txtView = view.findViewById(R.id.txtView);
         tvChoice = view.findViewById(R.id.tvChoice);
         mvChoice = view.findViewById(R.id.mvChoice);
-        radioGroup = (RadioGroup)view.findViewById(R.id.raidoButton);
+        radioGroup = (RadioGroup) view.findViewById(R.id.raidoButton);
 
         /*selectId =radioGroup.getCheckedRadioButtonId();
         radioButton = view.findViewById(selectId);*/
